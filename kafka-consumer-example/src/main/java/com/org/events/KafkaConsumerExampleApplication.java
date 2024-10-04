be3d47e94293cf5eaad53c0d209eac20c1727bb9
Amlan Deep Nandi <99658242+Amlandeep001@users.noreply.github.com>
2024-10-04 23:56:07 +0530

    Reformatting

--- kafka-consumer-example/src/main/java/com/org/events/KafkaConsumerExampleApplication.java
+++ kafka-consumer-example/src/main/java/com/org/events/KafkaConsumerExampleApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class KafkaConsumerExampleApplication {
-
-	public static void main(String[] args) {
+public class KafkaConsumerExampleApplication
+{
+	public static void main(String[] args)
+	{
 		SpringApplication.run(KafkaConsumerExampleApplication.class, args);
 	}
-
 }
